4ec93a64cab7e7fb8ce1afca42b4e9eec10c5e66
alecsey_efimov <vezenye44@yandex.ru>
2023-07-11 15:27:14 +0300

    Добавили DetailsFragment.kt и провели тестирование.

--- app/src/main/java/com/geekbrains/tests/view/details/DetailsActivity.kt
+++ app/src/main/java/com/geekbrains/tests/view/details/DetailsActivity.kt
@@ -5,39 +5,18 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.geekbrains.tests.R
-import com.geekbrains.tests.databinding.ActivityDetailsBinding
-import com.geekbrains.tests.presenter.details.DetailsPresenter
-import com.geekbrains.tests.presenter.details.PresenterDetailsContract
-import java.util.*
 
-class DetailsActivity : AppCompatActivity(), ViewDetailsContract {
-
-    private lateinit var binding: ActivityDetailsBinding
-
-    private val presenter: PresenterDetailsContract = DetailsPresenter(this)
+class DetailsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityDetailsBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        setUI()
-    }
-
-    private fun setUI() {
-        val count = intent.getIntExtra(TOTAL_COUNT_EXTRA, 0)
-        presenter.setCounter(count)
-        setCountText(count)
-        binding.decrementButton.setOnClickListener { presenter.onDecrement() }
-        binding.incrementButton.setOnClickListener { presenter.onIncrement() }
-    }
-
-    override fun setCount(count: Int) {
-        setCountText(count)
-    }
-
-    private fun setCountText(count: Int) {
-        binding.totalCountTextView.text =
-            String.format(Locale.getDefault(), getString(R.string.results_count), count)
+        setContentView(R.layout.activity_details)
+        supportFragmentManager.beginTransaction()
+            .add(
+                R.id.detailsFragmentContainer,
+                DetailsFragment.newInstance(intent.getIntExtra(TOTAL_COUNT_EXTRA, 0))
+            )
+            .commitAllowingStateLoss()
     }
 
     companion object {
